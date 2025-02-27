613db97973c590173cb2bb41df633db30503360a
neandrson <a2361@yandex.ru>
2025-02-27 21:41:54 +0300

    Update storage.go

--- orchestrator/use_cases/update_calculated/storage.go
+++ orchestrator/use_cases/update_calculated/storage.go
@@ -1,6 +1,6 @@
 package updatecalculated
 
-import "github.com/evsedov/GoCalculator/orchestrator/entities"
+import "github.com/neandrson/go-daev4/orchestrator/entities"
 
 type ExpressionUpdater interface {
 	UpdateCalculated(expression *entities.Expression) error
