7ef13784d1b08d99824a83182119fcec3e4b91ad
neandrson <a2361@yandex.ru>
2025-02-27 21:41:42 +0300

    Update storage.go

--- orchestrator/use_cases/login/storage.go
+++ orchestrator/use_cases/login/storage.go
@@ -1,6 +1,6 @@
 package login
 
-import "github.com/evsedov/GoCalculator/orchestrator/entities"
+import "github.com/neandrson/go-daev4/orchestrator/entities"
 
 type UserLogin interface {
 	Login(user *entities.User) ([]byte, error)
